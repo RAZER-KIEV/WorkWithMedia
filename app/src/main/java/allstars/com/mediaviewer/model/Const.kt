982b6ae40f9f3ed041a337e04fc755bd9f1af4db
RAZER <sdlan-razer@ukr.net>
2018-12-03 19:10:24 +0200

    hotfix

--- app/src/main/java/allstars/com/mediaviewer/model/Const.kt
+++ app/src/main/java/allstars/com/mediaviewer/model/Const.kt
@@ -2,7 +2,7 @@ package allstars.com.mediaviewer.model
 
 const val SHPREF_NAME = "settings"
 const val EXTRA_VIEW_TIME_SEC = "EXTRA_VIEW_TIME_SEC"
-const val VIEW_TIME_SEC = 5
+const val VIEW_TIME_SEC = 10
 const val EXTRA_COUNTER = "EXTRA_COUNTER"
 const val MENU_SETTINGS_ID = 1
 const val ANIMATIONS_DURATION = 1000L
